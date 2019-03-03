3c89e27f21ea527d4d6e284491ca9f9163c763e4
Nikunj <nikunj.parasar@gmail.com>
2019-03-02 17:16:18 -0800

    python-html hack

--- Main.py
+++ Main.py
@@ -71,3 +71,46 @@ def break_task(task):
 
 def open_website(url):
 	webbrowser.open(url=url)
+#!/usr/bin/env python
+import os
+
+headers = ["Content-type: text/html"]
+qs = os.environ['QUERY_STRING']
+
+def sendHeaders():
+    for h in headers:
+        print h
+    print "\n"
+
+def sendForm():
+    print '''
+    <html>
+      <body>
+          <form action='cgi-bin/hellobetter.py' method='get'>
+              <label for="myname">Enter Your Name</label>
+              <input id="myname" type="text" name="firstname" value="Nada" />
+              <input type="submit">
+          </form>
+      </body>
+    </html>
+    '''
+
+def sendPage(name):
+    print '''
+    <html>
+      <body>
+        <h1>Hello {0}</h1>
+      </body>
+    </html>
+    '''.format(name)
+
+if not qs:
+    sendHeaders()
+    sendForm()
+else:
+    if 'firstname' in qs:
+        name = qs.split('=')[1]
+    else:
+        name = 'No Name Provided'
+    sendHeaders()
+    sendPage(name)
